ea3465af7401a240b4a87c7ddb06203d1c4c3d54
JBMalveda <jbmalveda.pupc@gmail.com>
2020-11-29 08:06:38 +0800

    Added File model and relations

---
+++ app/File.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Subfolder;
+
+class File extends Model
+{
+    public function subfolder()
+    {
+        return $this->belongsTo(Subfolder::class);
+    }    
+}
--- app/Subfolder.php
+++ app/Subfolder.php
@@ -4,11 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Folder;
+use App\File;
 
 class Subfolder extends Model
 {
     public function folder()
     {
         return $this->belongsTo(Folder::class);
+    }
+
+    public function files()
+    {
+        return $this->hasMany(File::class);
     }
 }
