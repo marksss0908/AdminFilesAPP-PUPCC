80ef4a93f9f32b32fa9fbf94327d131e5135e2db
marksss0908 <anthony.bautista0908@gmail.com>
2020-12-12 11:23:45 +0800

    change pgsql syntax error

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Str;
-
+$DATABASE_URL=parse_url(‘DATABASE_URL’);
 return [
 
     /*
@@ -68,7 +68,7 @@ return [
             'url' => env('DATABASE_URL'),
             'host' => $DATABASE_URL["host"],
             'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URLP["path"], "/"),
+            'database' => ltrim($DATABASE_URL["path"], "/"),
             'username' => $DATABASE_URL["user"],
             'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
