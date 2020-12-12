55518e930aa1177e6a550dccd499adaaa0e2b5f6
marksss0908 <anthony.bautista0908@gmail.com>
2020-12-12 10:07:19 +0800

    added the Procfile

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 
 
-
+Route::get('/', 'DashboardController@index')->name('dashboard.index');
 
 //dashboard
 Route::get('/dashboard', 'DashboardController@index')->name('dashboard.index');
