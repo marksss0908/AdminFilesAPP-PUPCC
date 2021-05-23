e22e583fd58bfdc5c4d0e30116bdadd6536d9d4f
marksss0908 <anthony.bautista0908@gmail.com>
2021-05-24 00:07:14 +0800

    add permission Academic-head to role Academic-head

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -40,8 +40,8 @@ class AppServiceProvider extends ServiceProvider
         /* Add Permission to Role*/
         /* Select the Role and Permision 1 by 1 and give the permission*/
 
-            $role = Role::findorfail(4);
-            $permission = Permission::findorfail(3);
+            $role = Role::findorfail(5);
+            $permission = Permission::findorfail(4);
             $role->givePermissionTo($permission);
 
 
